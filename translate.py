486eae8a0f077142629afdc4089878c3da83a5f1
vijay <sahaniv66@gmail.com>
2020-12-30 12:17:50 +0530

    google langauage Translator

---
+++ translate.py
@@ -0,0 +1,13 @@
+from googletrans import Translator 
+
+f=open('input.txt','r')
+
+contents=f.read()
+
+file_translate = Translator()
+
+result = file_translate.translate(contents, dest='es')
+#print(result.text)
+
+with open('output.txt', 'w') as f:
+    f.write(result.text)
